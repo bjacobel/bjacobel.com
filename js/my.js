d26a3b89144c23e093a287606bee64a1cc05b93a
Brian Jacobel <bjacobel@gmail.com>
2013-10-24 17:24:29 -0400

    textures and shadows, begone.

--- js/my.js
+++ js/my.js
@@ -1,56 +1,46 @@
 //jQuery to expand the content drawer and fade elements
 $(document).ready(function(){
-    $(".navitem").click(function() {
+    $(".nav-item").click(function() {
         //read which tile is active from session storage
         var activeTile = sessionStorage.getItem("activeTile");
 
         //if expanded & the active tile is clicked on again, close it, set everyone back to bright
-        if ($("#contentdrawer").css("height")!="0px" && $(this).attr("tag")==activeTile){
+        if ($("#content-drawer").css("height")!="0px" && $(this).attr("tag")==activeTile){
             //collapse the backdrop
-            $(".contentbackdrop").css("height", "0%");
-
-            //hide all content - set html equal to ""
-            $(".content").html("");
+            $(".content-backdrop").css("height", "0%");
 
             //get rid of the arrow
             $(".arrow").css("visibility","hidden");
 
             setTimeout(function() {
                 //collapse the drawer after the content is gone
-                $("#contentdrawer").css("height", "0px");
+                $("#content-drawer").css("height", "0px");
 
-                //delay putting the shadow back until the drawer is closed
-                setTimeout(function() { 
-                    $("#navshadow").addClass("bottomshadow"); 
-                }, 250);
-            }, 175);
+                //hide all content - set html equal to ""
+                $(".content").html("");
+            }, 130);
 
             //set no tile as active
             sessionStorage.setItem("activeTile", -1);
         }
 
         //if not expanded and no other tile is active, open it
-        else if ($("#contentdrawer").css("height")=="0px" && activeTile==-1) {
+        else if ($("#content-drawer").css("height")=="0px" && activeTile==-1) {
+            console.log("asdf");
             //open the drawer
-            $("#contentdrawer").css("height", "40%");
+            $("#content-drawer").css("height", "40%");
 
-            //take the shadow off the bottom of the navbar
-            $("#navshadow").removeClass("bottomshadow");
-
-            var thistile = $(this);
+            thistile = $(this);
 
             //only do this stuff after the drawer is open
             setTimeout(function() {
                 //put up the backdrop and make the arrow visible on the correct tab
-                $(".contentbackdrop").css("height", "82%");
+                $(".content-backdrop").css("height", "82%");
                 $(".arrow").css("left", arrowPos($(thistile).attr("tag")));
                 $(".arrow").css("visibility", "visible");
 
                 //make the right content visible
                 $(".content").load($(thistile).attr("exthtml"));
-                require("js/swipeview.js", function () {
-                    SwipeView($("#swipeview-slider"));
-                });
             }, 150);
 
             //set this tile as active
@@ -58,11 +48,11 @@ $(document).ready(function(){
         }
 
         //if expanded & a new tile is clicked, switch the tile focus but don't do anything to the drawer
-        else if ($("#contentdrawer").css("height")!="0px" && activeTile!=-1 && activeTile!=$(this).attr("tag")){
+        else if ($("#content-drawer").css("height")!="0px" && activeTile!=-1 && activeTile!=$(this).attr("tag")){
             //move the arrow
             $(".arrow").css("left", arrowPos($(this).attr("tag")));
 
-            var thistile = $(this);
+            thistile = $(this);
 
             //make the right content visible
             $(".content").load($(thistile).attr("exthtml"));
@@ -82,14 +72,14 @@ function setupStorage() {
 function arrowPos(tile) {
     switch (tile) {
         case "0":
-        return "10%"; break;
+            return "10%";
         case "1":
-        return "35.5%"; break;
+            return "35.5%";
         case "2":
-        return "61%"; break;
+            return "61%";
         case "3":
-        return "87%"; break;
+            return "87%";
         default:
-        return null;
+            return null;
     }
 }
