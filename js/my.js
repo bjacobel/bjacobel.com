9511ccfd85d103af28812cde8f1d9e2632cd8303
Brian Jacobel <bjacobel@gmail.com>
2013-10-24 14:36:19 -0400

    Code cleanup - no visual changes yet

--- js/my.js
+++ js/my.js
@@ -1,153 +1,95 @@
 //jQuery to expand the content drawer and fade elements
 $(document).ready(function(){
-	$(".navitem").click(function() {
-		//read which tile is active from session storage
-		var activeTile = sessionStorage.getItem("activeTile");
+    $(".navitem").click(function() {
+        //read which tile is active from session storage
+        var activeTile = sessionStorage.getItem("activeTile");
 
-		//if expanded & the active tile is clicked on again, close it, set everyone back to bright
-		if ($("#contentdrawer").css("height")!="0px" && $(this).attr("tag")==activeTile){
-		    //collapse the backdrop
-		    $(".contentbackdrop").css("height", "0%");
+        //if expanded & the active tile is clicked on again, close it, set everyone back to bright
+        if ($("#contentdrawer").css("height")!="0px" && $(this).attr("tag")==activeTile){
+            //collapse the backdrop
+            $(".contentbackdrop").css("height", "0%");
 
-		    //hide all content - set html equal to ""
-		    $(".content").html("");
+            //hide all content - set html equal to ""
+            $(".content").html("");
 
-		    //get rid of the arrow
-		    $(".arrow").css("visibility","hidden");
+            //get rid of the arrow
+            $(".arrow").css("visibility","hidden");
 
-		    setTimeout(function() {
-			//collapse the drawer after the content is gone
-			$("#contentdrawer").css("height", "0px");
+            setTimeout(function() {
+                //collapse the drawer after the content is gone
+                $("#contentdrawer").css("height", "0px");
 
-			//delay putting the shadow back until the drawer is closed
-			setTimeout(function() { $("#navshadow").addClass("bottomshadow"); }, 250);
-		}, 175);
+                //delay putting the shadow back until the drawer is closed
+                setTimeout(function() { 
+                    $("#navshadow").addClass("bottomshadow"); 
+                }, 250);
+            }, 175);
 
-		    //set no tile as active
-		    sessionStorage.setItem("activeTile", -1);
-		}
+            //set no tile as active
+            sessionStorage.setItem("activeTile", -1);
+        }
 
-		//if not expanded and no other tile is active, open it
-		else if ($("#contentdrawer").css("height")=="0px" && activeTile==-1) {
-		    //open the drawer
-		    $("#contentdrawer").css("height", "40%");
+        //if not expanded and no other tile is active, open it
+        else if ($("#contentdrawer").css("height")=="0px" && activeTile==-1) {
+            //open the drawer
+            $("#contentdrawer").css("height", "40%");
 
-		    //take the shadow off the bottom of the navbar
-		    $("#navshadow").removeClass("bottomshadow");
+            //take the shadow off the bottom of the navbar
+            $("#navshadow").removeClass("bottomshadow");
 
-		    var thistile = $(this);
+            var thistile = $(this);
 
-		    //only do this stuff after the drawer is open
-		    setTimeout(function() {
-			//put up the backdrop and make the arrow visible on the correct tab
-			$(".contentbackdrop").css("height", "82%");
-			$(".arrow").css("left", arrowPos($(thistile).attr("tag")));
-			$(".arrow").css("visibility", "visible");
+            //only do this stuff after the drawer is open
+            setTimeout(function() {
+                //put up the backdrop and make the arrow visible on the correct tab
+                $(".contentbackdrop").css("height", "82%");
+                $(".arrow").css("left", arrowPos($(thistile).attr("tag")));
+                $(".arrow").css("visibility", "visible");
 
-			//make the right content visible
-			$(".content").load($(thistile).attr("exthtml"));
-			require("js/swipeview.js", function () {
-    			SwipeView($("#swipeview-slider"));
-    		});
-		}, 150);
+                //make the right content visible
+                $(".content").load($(thistile).attr("exthtml"));
+                require("js/swipeview.js", function () {
+                    SwipeView($("#swipeview-slider"));
+                });
+            }, 150);
 
-		    //set this tile as active
-		    sessionStorage.setItem("activeTile", $(this).attr("tag"));
-		}
+            //set this tile as active
+            sessionStorage.setItem("activeTile", $(this).attr("tag"));
+        }
 
-		//if expanded & a new tile is clicked, switch the tile focus but don't do anything to the drawer
-		else if ($("#contentdrawer").css("height")!="0px" && activeTile!=-1 && activeTile!=$(this).attr("tag")){
-		    //move the arrow
-		    $(".arrow").css("left", arrowPos($(this).attr("tag")));
+        //if expanded & a new tile is clicked, switch the tile focus but don't do anything to the drawer
+        else if ($("#contentdrawer").css("height")!="0px" && activeTile!=-1 && activeTile!=$(this).attr("tag")){
+            //move the arrow
+            $(".arrow").css("left", arrowPos($(this).attr("tag")));
 
-		    var thistile = $(this);
+            var thistile = $(this);
 
-		    //make the right content visible
-		    $(".content").load($(thistile).attr("exthtml"));
+            //make the right content visible
+            $(".content").load($(thistile).attr("exthtml"));
 
-		    //set this tile as active
-		    sessionStorage.setItem("activeTile", $(this).attr("tag"));
-		}
+            //set this tile as active
+            sessionStorage.setItem("activeTile", $(this).attr("tag"));
+        }
 
-	});
+    });
 });
 
 //stores a local variable... shitty coding alert (but it works)
 function setupStorage() {
-	sessionStorage.setItem("activeTile", -1);
+    sessionStorage.setItem("activeTile", -1);
 }
 
 function arrowPos(tile) {
-	switch (tile) {
-		case "0":
-		return "10%"; break;
-		case "1":
-		return "35.5%"; break;
-		case "2":
-		return "61%"; break;
-		case "3":
-		return "87%"; break;
-		default:
-		return null;
-	}
-}
-
-function require(file, callback) {
-
-    var script = document.getElementsByTagName('script')[0],
-        newjs = document.createElement('script');
-
-    // IE
-    newjs.onreadystatechange = function () {
-        if (newjs.readyState === 'loaded' || newjs.readyState === 'complete') {
-            callback();
-        }
-    };
-
-    // others
-    newjs.onload = function () {
-        callback();
-    };
-
-    newjs.src = file;
-    script.parentNode.insertBefore(newjs, script);
-}
-
-function runSwiper(){
-var	carousel,
-	el,
-	i,
-	page,
-	slides = [
-
-	];
-
-carousel = new SwipeView('#wrapper', {
-	numberOfPages: slides.length,
-	hastyPageFlip: true
-});
-
-// Load initial data
-for (i=0; i<3; i++) {
-	page = i==0 ? slides.length-1 : i-1;
-
-	el = document.createElement('span');
-	el.innerHTML = slides[page];
-	carousel.masterPages[i].appendChild(el)
-}
-
-carousel.onFlip(function () {
-	var el,
-		upcoming,
-		i;
-
-	for (i=0; i<3; i++) {
-		upcoming = carousel.masterPages[i].dataset.upcomingPageIndex;
-
-		if (upcoming != carousel.masterPages[i].dataset.pageIndex) {
-			el = carousel.masterPages[i].querySelector('span');
-			el.innerHTML = slides[upcoming];
-		}
-	}
-});
+    switch (tile) {
+        case "0":
+        return "10%"; break;
+        case "1":
+        return "35.5%"; break;
+        case "2":
+        return "61%"; break;
+        case "3":
+        return "87%"; break;
+        default:
+        return null;
+    }
 }
