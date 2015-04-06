51def291089cdb382835516979c3c78861c8cf15
Brian Jacobel <bjacobel@gmail.com>
2015-04-05 23:53:48 -0400

    Fastclick

--- js/my.js
+++ js/my.js
@@ -2,3 +2,7 @@ $(".mobile-burger").click(function(){
     $(".main").toggleClass("slide-visible");
     $(".burger-lines").toggleClass("active");
 });
+
+$(function() {
+    FastClick.attach(document.body);
+});
