b0054944df81071f7233f458cb06278ac4680bab
Brian Jacobel <bjacobel@gmail.com>
2017-02-08 15:14:31 -0500

    Add link to Keybase Chat

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -15,11 +15,16 @@ export default class Contact extends Component {
       <div>
         <h2 id="twitter"><a href="https://twitter.com/bjacobel">Twitter</a></h2>
         <p>My DMs are closed by default. Ask me to follow you first so we can DM.</p>
-        <p>Please be advised Twitter DMs don't implement any kind of E2E encryption.</p>
-        <h2 id="twitter"><a href="https://whispersystems.org/">Signal</a></h2>
+        <p>Please note Twitter DMs don't implement any kind of E2E encryption.</p>
+
+        <h2 id="signal"><a href="https://whispersystems.org/">Signal</a></h2>
         <p>Message me using another contact method first to ask for my Signal number.</p>
+
+        <h2 id="keybase"><a href="https://keybase.io/blog/keybase-chat">Keybase Chat</a></h2>
+        <p>I'm <a href="https://keybase.io/bjacobel">bjacobel</a> on Keybase.</p>
+
         <h2 id="pgp">Email</h2>
-        <p>Send me PGP-encrypted mail using <a href="https://keybase.io/bjacobel">keybase.io</a> or the info below.</p>
+        <p>You can also use Keybase to get my PGP key and send me email at the appropriate address below.</p>
         <ul>
           <li><Email user="bjacobel" domain="gmail.com" /> (personal)</li>
           <li><Email user="brian" domain="bjacobel.com" /> (professional)</li>
